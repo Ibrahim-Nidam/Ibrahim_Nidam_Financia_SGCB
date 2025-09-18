b5bd50aed7ceccbb2b70465a6e5ac7056576065a
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-09-18 09:35:47 +0100

    feat(utils): add DateUtils for date/time formatting and retrieval

--- src/utils/DateUtils.java
+++ src/utils/DateUtils.java
@@ -1,7 +1,16 @@
 package utils;
 
-
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
     
+    public static LocalDateTime getCurrentDateTime() {
+        return LocalDateTime.now();
+    }
+    
+    public static String formatDateTime(LocalDateTime dateTime) {
+        return dateTime.format(FORMATTER);
+    }
 }
