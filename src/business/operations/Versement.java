8f8bb5c4ad3367848bfa85d0f0bc95e4313a650f
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-09-18 10:17:21 +0100

    feat(operations): add Versement operation type

--- src/business/operations/Versement.java
+++ src/business/operations/Versement.java
@@ -1,5 +1,19 @@
 package business.operations;
 
 public class Versement extends Operation {
-   
+    private String source;
+    
+    public Versement(double montant, String source) {
+        super(montant);
+        this.source = source;
+    }
+    
+    public String getSource() { 
+        return source; 
+    }
+    
+    @Override
+    public String toString() {
+        return "VERSEMENT - " + super.toString() + " | Source: " + source;
+    }
 }
