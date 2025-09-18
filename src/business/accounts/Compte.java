de94c1c97f79ffa3ef89aea1e16db104b953fbfb
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-09-18 09:42:36 +0100

    feat(metier): add abstract Compte base class
    - Defined common attributes: code, solde, listeOperations
    - Added constructor to initialize account with code and initial balance
    - Declared abstract methods: retirer, calculerInteret, afficherDetails
    - Added getters and protected addOperation for subclasses

--- src/business/accounts/Compte.java
+++ src/business/accounts/Compte.java
@@ -1,7 +1,35 @@
 package business.accounts;
 
-
+import business.operations.Operation;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Compte {
+    protected String code;
+    protected double solde;
+    protected List<Operation> listeOperations;
     
+    public Compte(String code, double soldeInitial) {
+        this.code = code;
+        this.solde = soldeInitial;
+        this.listeOperations = new ArrayList<>();
+    }
+    
+    public abstract boolean retirer(double montant, String destination);
+    public abstract double calculerInteret();
+    public abstract void afficherDetails();
+    
+    public String getCode() { 
+        return code;
+    }
+    public double getSolde() { 
+        return solde; 
+    }
+    public List<Operation> getListeOperations() { 
+        return new ArrayList<>(listeOperations); 
+    }
+    
+    protected void addOperation(Operation operation) {
+        this.listeOperations.add(operation);
+    }
 }
